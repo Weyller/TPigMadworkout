9c1ade0637c03f0dc2a7ee5ecfe151ef4d72d141
eleves <eleves@mac404inf-09.local>
2017-07-17 12:02:50 -0400

    hideKeyboard

--- igMadworkout/ViewController.swift
+++ igMadworkout/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    
+    //-----------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //=====================================
+    
+    @IBAction func hideKeyboard(_ sender: UIButton) {
+        
+        self.view.endEditing(true)
+    }
+   
+    
 
 }
 
